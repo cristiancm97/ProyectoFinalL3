30a8a9ce679b0bf55f0b0ee23832c1701a087bbb
cristiancm97git <cristian_cm_16@outlook.com>
2024-06-29 03:51:22 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Registro from './componentes/pages/Registro';
 import Login from './componentes/pages/Home';
+import HomeUsuario from './componentes/pages/HomeUsuario';
+import HomeEmpleado from './componentes/pages/HomeEmpleado';
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Login/>}/>
           <Route path="/registro" element={<Registro/>}/>
+          <Route path="/user" element={<HomeUsuario/>}/>
+          <Route path="/admin" element={<HomeEmpleado/>}/>
         </Routes>
       </BrowserRouter>     
     </div>
---
+++ src/componentes/pages/HomeEmpleado.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import NavBar from '../NavBar/NavBar'
+
+function HomeEmpleado() {
+  return (
+    <div>
+      <NavBar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default HomeEmpleado
